cd3fc230882a462cef58472481d00e1305c8be43
Matheus Antonio Monteiro <matheusantoniomonteiro035@gmail.com>
2022-08-01 20:41:47 -0300

    Adding main and notion_functions modifications

--- main.py
+++ main.py
@@ -1,45 +1,4 @@
-from notion_functions import insert_values, generate_csv_and_clear_database
-import datetime
-
-def inserting():
-    print('Please inform the data for the insert:\n')
-
-    expense = input('Expense: ')
-    amount = float(input('Amount: '))
-    category = input('Category: ')
-    comment = input('Comment: ')
-    date = input('Date(pls use this format: yyyy-mm-dd or let it blank for get today): ')
-    status = input('Status(payed | to pay): ')
-
-    if date == "":
-        date = datetime.datetime.now().strftime('%Y-%m-%d')
-
-    page = {
-        "Expenses": expense,
-        "Amount": amount,
-        "Category": category,
-        "Comment": comment,
-        "Date": date,
-        "Status": status
-    }
-
-    insert_values(page)
-
-    op = ''
-    while op != 'n' or op != 'no':
-        op = input('\n Do you want to add more itens(y, yes | n, no): ')
-
-        if op == 'no' or op == 'n':
-            print('Ok! Thanks for use the system! See you soon\n')
-            break
-        elif op == 'yes' or op == 'y':
-            inserting()
-            break
-        else:
-            print('Wrong option, please choose y|yes or n|no\n')
-            continue
-
-    return None
+from notion_functions import inserting, generate_csv_and_clear_database
 
 print('------------------------------------------------------------\n')
 print('Choose what you want to do: ')
--- notion_functions.py
+++ notion_functions.py
@@ -7,6 +7,50 @@ insert_url = config.get('INSERT_URL')
 read_url = config.get('READ_URL')
 csv_file_path = config.get('CSVFILEPATH')
 
+def inserting():
+    print('Please inform the data for the insert:\n')
+
+    expense = input('Expense: ')
+    try:
+        amount = float(input('Amount: '))
+    except:
+        print("Amount is not a number, please try again")
+        inserting()
+    category = input('Category: ')
+    comment = input('Comment: ')
+    date = input('Date(pls use this format: yyyy-mm-dd or let it blank for get today): ')
+    status = 'payed'
+
+    if date == "":
+        date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    page = {
+        "Expenses": expense,
+        "Amount": amount,
+        "Category": category,
+        "Comment": comment,
+        "Date": date,
+        "Status": status
+    }
+
+    insert_values(page)
+
+    op = ''
+    while op != 'n' or op != 'no':
+        op = input('\n Do you want to add more itens(y, yes | n, no): ')
+
+        if op == 'no' or op == 'n':
+            print('Ok! Thanks for use the system! See you soon\n')
+            break
+        elif op == 'yes' or op == 'y':
+            inserting()
+            break
+        else:
+            print('Wrong option, please choose y|yes or n|no\n')
+            continue
+
+    return None
+
 def generate_csv_and_clear_database():
     res = requests.post(read_url, headers=header)
 
